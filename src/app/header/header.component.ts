b9f87a780b6f7024d1f4d0e2ec39baab343d04c4
steidler-eu <81076463+steidler-eu@users.noreply.github.com>
2024-04-23 21:49:47 +0000

    added stuff

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ThemeToggleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/header/theme-toggle/theme-toggle.component.ts
+++ src/app/header/theme-toggle/theme-toggle.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../theme.service';
 
 @Component({
   selector: 'app-theme-toggle',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './theme-toggle.component.scss'
 })
 export class ThemeToggleComponent {
+
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 
 }
