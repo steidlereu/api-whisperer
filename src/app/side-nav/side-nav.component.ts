50118e261dbc6d8c178312a78b9f40900cde3b98
steidler-eu <81076463+steidler-eu@users.noreply.github.com>
2024-04-30 22:13:12 +0000

    added stuff

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
   imports: [
     CollapseModule,
+    TooltipModule
   ],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
