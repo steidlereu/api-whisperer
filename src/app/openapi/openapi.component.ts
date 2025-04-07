5755301fc2efa459d370985a29457fc0f0b53170
steidler-eu <81076463+steidler-eu@users.noreply.github.com>
2025-04-07 21:48:43 +0000

    added search for active product

--- src/app/openapi/openapi.component.ts
+++ src/app/openapi/openapi.component.ts
@@ -7,6 +7,7 @@ import {TabDirective, TabsModule} from "ngx-bootstrap/tabs";
 import {MarkdownComponent} from "ngx-markdown";
 import { ConfigService } from '../services/config.service'; // Adjust the path as necessary
 import { SettingsService } from '../services/settings.service'; // Adjust the path as necessary
+import { Product } from '../models/Product';
 
 @Component({
     selector: 'app-openapi',
@@ -18,17 +19,34 @@ import { SettingsService } from '../services/settings.service'; // Adjust the pa
 export class OpenapiComponent implements OnInit {
 
   @ViewChild(YamlEditorComponent) yamlEditor!: YamlEditorComponent
+
+  activeProduct: Product | null = null;
   
   constructor(
       private configService: ConfigService,
       private settingsService: SettingsService
     ) { }
 
+  private loadActiveProduct(): void {
+
+    const products = this.configService.getConfig()?.products || [];
+
+    for (const explorerElement of this.settingsService.loadSettings().explorer.elements) {
+
+      if (this.settingsService.countExplorerElementDeep(explorerElement.name) === 1) {
+        if (explorerElement.active) {
+          const product = products.find((product) => product.name === explorerElement.name);
+          alert('Product found: ' + product?.name);
+        }
+      }
+
+    }
+  }
 
   ngOnInit(): void {
     this.settingsService.storageChange$.subscribe((data) => {
-      if (data.key === 'app_settings') {
-        alert('Settings changed:' + data.value);
+      if (data.key === 'app_settings') {  
+        this.loadActiveProduct();
       }
     });
   }
