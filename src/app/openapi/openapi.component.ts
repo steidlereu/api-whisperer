bbf7b3028d479f73c26b41d1eb24bd64bc66f4f3
steidler-eu <81076463+steidler-eu@users.noreply.github.com>
2025-04-07 14:57:01 +0000

    addd stuff

--- src/app/openapi/openapi.component.ts
+++ src/app/openapi/openapi.component.ts
@@ -1,10 +1,12 @@
-import {Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { YamlEditorComponent } from "./yaml-editor/yaml-editor.component";
 import { WorkspaceComponent } from "./workspace/workspace.component";
 import { SwaggerUiComponent } from './swagger-ui/swagger-ui.component';
 import {NgIf} from "@angular/common";
 import {TabDirective, TabsModule} from "ngx-bootstrap/tabs";
 import {MarkdownComponent} from "ngx-markdown";
+import { ConfigService } from '../services/config.service'; // Adjust the path as necessary
+import { SettingsService } from '../services/settings.service'; // Adjust the path as necessary
 
 @Component({
     selector: 'app-openapi',
@@ -13,9 +15,23 @@ import {MarkdownComponent} from "ngx-markdown";
     styleUrl: './openapi.component.scss',
   imports: [YamlEditorComponent, WorkspaceComponent, SwaggerUiComponent, NgIf, TabsModule, MarkdownComponent]
 })
-export class OpenapiComponent {
+export class OpenapiComponent implements AfterViewInit {
 
-  @ViewChild(YamlEditorComponent) yamlEditor!: YamlEditorComponent;
+  @ViewChild(YamlEditorComponent) yamlEditor!: YamlEditorComponent
+  
+  constructor(
+      private configService: ConfigService,
+      private settingsService: SettingsService
+    ) { }
+    
+  ngAfterViewInit(): void {
+    console.log('OpenapiComponent initialized');
+    window.addEventListener('storage', (event) => {
+      if (event.key === 'app_settings') {
+        console.log('Der Wert von yourKey hat sich geändert:' + event.newValue);
+      }
+    });// Event-Listener für Änderungen im localStorage
+  }
 
   onSelect(data: TabDirective): void {
     setTimeout(() => {
