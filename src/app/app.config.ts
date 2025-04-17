d2dbd2e2886cc1881b9c7df0822e4093141cf5f7
steidler-eu <81076463+steidler-eu@users.noreply.github.com>
2025-04-17 14:28:46 +0000

    added basics for dynamic navigations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {APP_INITIALIZER, ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
@@ -13,18 +13,32 @@ export function initializeApp(configService: ConfigService) {
   return () => configService.loadConfig();
 }
 
+export function generateRoutes(configService: ConfigService) {
+  return async () => {
+    const dynamicRoutes = configService.getConfig();
+
+    provideRouter(routes);
+  };
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
       deps: [ConfigService],
       multi: true, // Ensures it runs before app starts
     },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: generateRoutes,
+      deps: [ConfigService],
+      multi: true,
+    },
+    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     provideAnimations(),
     importProvidersFrom(
       BsDropdownModule.forRoot(),
